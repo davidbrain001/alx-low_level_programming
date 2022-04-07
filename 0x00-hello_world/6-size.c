e49e76b8a71b3b2ddf05fb172f02590aed4d5f74
davidbrain001 <davidbabatundeadeoye@gmail.com>
2022-04-07 12:24:53 -0700

    My task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -13,3 +13,4 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
+
