bcbd1f8e0ad88d3272ec90a4944176f2ca26473b
davidbrain001 <davidbabatundeadeoye@gmail.com>
2022-04-09 09:30:15 -0700

    My task 3 commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,19 @@
 int main(void)
 
 {
-char alphabet;
+char lower_case;
+char upper_case;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+  {
+    putchar(lower_case);
+  }
 
-putchar('\n');
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+  {
+    putchar(upper_case);
+  }
 
-return (0);
 
-}
-
+   
+   
