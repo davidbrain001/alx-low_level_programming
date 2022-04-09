2e4d91244dfc14ad413507ef521c7a0a4589fd28
davidbrain001 <davidbabatundeadeoye@gmail.com>
2022-04-09 09:46:57 -0700

    My task 3 commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,15 +11,15 @@ char lower_case;
 char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-  {
-    putchar(lower_case);
-  }
+{
+putchar(lower_case);
+}
 
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-  {
-    putchar(upper_case);
-  }
+{
+putchar(upper_case);
 }
+
 
 
 
