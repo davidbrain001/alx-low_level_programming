aab9e8e6195dd91d96bb9d085564feaa7b3b05f5
davidbrain001 <davidbabatundeadeoye@gmail.com>
2022-05-11 15:35:50 -0700

    My task 0 commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-my readme file
+My first Readme
--- main.h
+++ main.h
@@ -1,12 +1,10 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif
