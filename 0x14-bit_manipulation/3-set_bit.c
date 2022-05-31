f415fdcb3dfd9609b55a79202ce3de5aad8daf5c
davidbrain001 <davidbabatundeadeoye@gmail.com>
2022-05-31 01:12:19 -0700

    My task 3 commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
- * @index: index of the bit
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
  *
- * Return: value of the bit
+ * Return: 1 if it worked, -1 if it didn't.
  */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-    int bit_val;
+  unsigned int a;
 
-    if (index > 63)
-        return (-1);
+  if (index > 63)
+    return (-1);
 
-    bit_val = (n >> index) & 1;
+  a = 1 << index;
+  *n = (*n | a);
 
-    return (bit_val);
+  return (1);
 }
